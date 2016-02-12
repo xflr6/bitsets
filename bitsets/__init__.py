a27ccc4f9d34a037a46253305aa4a81ac970394c
Sebastian Bank <sebastian.bank@uni-leipzig.de>
2016-02-12 15:02:19 +0100

    bump version for development

--- bitsets/__init__.py
+++ bitsets/__init__.py
@@ -3,7 +3,7 @@
 """Ordered subsets of a predetermined finite domain."""
 
 __title__ = 'bitsets'
-__version__ = '0.7.10'
+__version__ = '0.7.11.dev0'
 __author__ = 'Sebastian Bank <sebastian.bank@uni-leipzig.de>'
 __license__ = 'MIT, see LICENSE'
 __copyright__ = 'Copyright (c) 2013-2016 Sebastian Bank'
