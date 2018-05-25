761fe1ed76d38b31beedb0f1ff8bec6c13cb6c4c
Sebastian Bank <sebastian.bank@uni-leipzig.de>
2018-05-25 18:58:05 +0200

    bump version for development

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='bitsets',
-    version='0.7.13',
+    version='0.7.14.dev0',
     author='Sebastian Bank',
     author_email='sebastian.bank@uni-leipzig.de',
     description='Ordered subsets over a predefined domain',
