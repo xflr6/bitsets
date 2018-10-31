da9c067a68f2b72f7f7dfb2063e831f606ffb1b6
Sebastian Bank <sebastian.bank@uni-leipzig.de>
2018-10-31 20:09:06 +0100

    release 0.7.14

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='bitsets',
-    version='0.7.14.dev0',
+    version='0.7.14',
     author='Sebastian Bank',
     author_email='sebastian.bank@uni-leipzig.de',
     description='Ordered subsets over a predefined domain',
